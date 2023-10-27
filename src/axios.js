5ea74bdff2bd1f7eec1142d98058c716ef9d43e3
Sanjana Chathli <103053016+sanjanachathli@users.noreply.github.com>
2023-10-27 12:53:20 +0530

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: 'https://nour-al-dunya-api.onrender.com/api/v1'
+  baseURL: 'https://nour-al-dunya-api.onrender.com'
 //   baseURL: 'https://good-jade-betta-ring.cyclic.app'
 })
 
