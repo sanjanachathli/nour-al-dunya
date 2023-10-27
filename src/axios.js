f03f8a92b803f60fb91b496ab81f084c1d048623
sanjanachathli <sanjanachathli4@gmail.com>
2023-10-27 11:15:48 +0530

    added backend url

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: 'http://localhost:4000/api/v1'
+  baseURL: 'https://nour-al-dunya-api.onrender.com/api/v1'
 //   baseURL: 'https://good-jade-betta-ring.cyclic.app'
 })
 
